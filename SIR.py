bc011f3accabffd309e38923b3fba06ce90dbd52
Matthew Martin <matthewmartin117@gmail.com>
2024-08-15 17:08:17 -0400

    front end skeleton and flask app

---
+++ SIR.py
@@ -0,0 +1,17 @@
+# imported the Flask class; instance of this class will be the app
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+#Decorate a view function to register it with the given URL rule and options.
+
+
+# this will take to the main page
+@app.route("/")
+def hello_world():
+  return render_template('model.html')
+
+
+@app.route("/about")
+def about_page():
+  return "<h1>This is my about page<h1>"
